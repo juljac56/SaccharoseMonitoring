0fe23405fafe7c3b1d07c01a6b3b976d8c60c7c4
Julien Jacquot <juljacquot@orange.fr>
2021-11-14 19:59:34 +0100

    login interface ok

--- HelloEI/src/main/java/com/example/saccaei/HelloController.java
+++ HelloEI/src/main/java/com/example/saccaei/HelloController.java
@@ -9,11 +9,15 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class HelloController {
     public Stage stage;
@@ -24,22 +28,44 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
+    TextField username;
+    @FXML
+    PasswordField password;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
     @FXML
     protected void loginOK(ActionEvent event) {
-        System.out.println("Login Ok");
+        System.out.println("Tentative login");
         try {
-            root = FXMLLoader.load(getClass().getResource("linear.fxml"));
+
             Connection conn = DataBase.getConnection();
-        } catch (IOException e) {System.out.println(e);}
+            String user = username.getText();
+            String mdp = password.getText();
 
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+            // on cherche les id des auteurs du nouveau livre
+            PreparedStatement ps = conn.prepareStatement("Select * FROM MEDECIN WHERE username = ? AND mdp = ?");
+            ps.setString(1, user);
+            ps.setString(2, mdp);
+            ResultSet rs = ps.executeQuery();
+
+            if (!rs.isBeforeFirst() ) {
+                System.out.println("pas de connexion");
+                root = FXMLLoader.load(getClass().getResource("login.fxml"));}
+            else{
+                System.out.println("Connexion ok");
+                root = FXMLLoader.load(getClass().getResource("linear.fxml"));
+            }
+
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {System.out.println(e);}
+
     }
-
 }
